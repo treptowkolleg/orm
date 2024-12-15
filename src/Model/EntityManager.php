b88124495b165087a838d0b00521d0c7f734b233
AG Informatik <123292918+treptowkolleg@users.noreply.github.com>
2024-12-15 21:36:38 +0100

    update

--- src/Model/EntityManager.php
+++ src/Model/EntityManager.php
@@ -219,7 +219,7 @@ class EntityManager
 
         foreach($this->parameters as $key => $value) {
             $statement->bindValue($key, $value);
-            if (defined('DEBUG')) echo "$key: $value\n";
+            if(defined('DEBUG')) echo "$key: $value\n";
         }
         if($update) $statement->bindValue(":" . key($this->primaryKey),$this->primaryKey[key($this->primaryKey)]);
         $statement->execute();
